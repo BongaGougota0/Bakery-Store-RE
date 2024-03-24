ad7c13b240c7816608bfca6882f0451d2a6136e2
Bonga Gougota <Bonga.Gougota@rmb.co.za>
2024-03-24 19:04:27 +0200

    in  OrderItem comment out Min(1) constraint, Error for some reason. Current running version.

--- Part_001/P001/vaadin-flow-springboot/src/main/java/za/co/bakery/backend/data/entity/OrderItem.java
+++ Part_001/P001/vaadin-flow-springboot/src/main/java/za/co/bakery/backend/data/entity/OrderItem.java
@@ -1,20 +1,46 @@
 package za.co.bakery.backend.data.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Entity
-@Setter
-@Getter
 public class OrderItem extends AbstractDataEntity{
-    private Product product;
-    @Min(1)
+    @ManyToOne
     @NotNull
-    private Integer quantity = Integer.valueOf(1);
+    private Product product;
+
+//    @Min(1)
+    @NotNull
+    private Integer quantity = 1;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
     @Size(max = 255)
     private String comment;
 
